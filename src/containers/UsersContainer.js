3444a879297ece326b3a6b8c4151d2ac808d9ad0
ensallee <ensallee@gmail.com>
2018-07-30 13:26:22 -0400

    added searching field to community page

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import NavBar3 from '../components/NavBar3'
 import User from '../components/User'
-import { Card } from 'semantic-ui-react'
+import { Card, Input } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { setAllUsers } from '../actions';
 
 class UsersContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state={
+      seachTerm: "",
+      filteredUsers: []
+    }
+  }
   handleFollow = (id) => {
     console.log('inside handleFollow', id)
 
@@ -50,17 +58,42 @@ class UsersContainer extends Component {
     })
   }
 
+  handleSearch = (e) => {
+    let searchTerm = e.target.value
+    let filteredUsers = this.props.users.filter(u => {
+      return u.display_name.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+    console.log('filteredUsers', filteredUsers)
+    this.setState({
+      searchTerm: searchTerm,
+      filteredUsers: filteredUsers
+    }, () => console.log('search Term inside state', this.state.searchTerm))
+  }
+
   render() {
     let userComponents = this.props.users.map(u => {
-      return <User user = {u} history = {this.props.history} unFollow={this.unFollow} handleFollow={this.handleFollow} followers={u.followers} following={u.following} key={u.id} id={u.id} name={u.display_name} bio={u.bio} hometown={u.hometown} />
-    })
+        return <User user = {u} history = {this.props.history} unFollow={this.unFollow} handleFollow={this.handleFollow} followers={u.followers} following={u.following} key={u.id} id={u.id} name={u.display_name} bio={u.bio} hometown={u.hometown} />
+      })
+    let filteredUsers = this.state.filteredUsers.map(u => {
+        return <User user = {u} history = {this.props.history} unFollow={this.unFollow} handleFollow={this.handleFollow} followers={u.followers} following={u.following} key={u.id} id={u.id} name={u.display_name} bio={u.bio} hometown={u.hometown} />
+      })
     return (
       <Fragment>
         <NavBar3 />
         <div className="users-container">
         <h1>All Poets</h1>
+          <Input
+            icon='users'
+            iconPosition='left'
+            style={{padding:'6px', margin: '5px'}}
+            size='large'
+            onChange={this.handleSearch}
+            placeholder='Search Poets'
+            value={this.state.searchTerm}
+            type="text"
+          />
           <Card.Group centered itemsPerRow={4}>
-            {userComponents}
+            {this.state.filteredUsers.length !== 0 ? <Fragment>{filteredUsers}</Fragment> : <Fragment>{userComponents}</Fragment>}
           </Card.Group>
         </div>
       </Fragment>
